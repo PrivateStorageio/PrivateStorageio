e2baee6c4d57afc582d1544076669bd9eea4f9b4
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-08-07 13:55:08 -0400

    Make the test pass by waiting a little longer.
    Also, comments.

--- nixos/modules/tests/private-storage.nix
+++ nixos/modules/tests/private-storage.nix
@@ -13,8 +13,19 @@ import <nixpkgs/nixos/tests/make-test.nix> {
   # Test the machine with a Perl program (sobbing).
   testScript =
     ''
+      # Boot the VM.
       $machine->start;
+
+      # The systemd unit should reach the running state.
       $machine->waitForUnit("tahoe.storage.service");
+
+      # Some while after that the Tahoe-LAFS node should listen on the web API
+      # port. The port number here has to agree with the port number set in
+      # the private-storage.nix module.
+      $machine->waitForOpenPort(3456);
+
+      # Once the web API is listening it should be possible to scrape some
+      # status from the node if it is really working.
       $machine->succeed("tahoe -d /var/db/tahoe-lafs/storage status");
     '';
 }
