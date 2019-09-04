f3192716f84bc1e508e5ded47694b9882ad2c180
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-04 16:37:10 -0400

    expand this comment slightly

--- morph/grid.nix
+++ morph/grid.nix
@@ -2,7 +2,8 @@ let
   # Pin the deployment package-set to a specific version of nixpkgs.  This is
   # NixOS 19.03 as of Aug 28 2019.  There's nothing special about it.  It's
   # just recent at the time of development.  It can be upgraded when there is
-  # value in doing so.
+  # value in doing so.  Meanwhile, our platform doesn't shift around beneath
+  # us in surprising ways as time passes.
   pkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/3c83ad6ac13b67101cc3e2e07781963a010c1624.tar.gz";
     sha256 = "0cdq342wrkvkyccygpp1gvwp7hhqg68hljjwld4vjixm901ayy14";
