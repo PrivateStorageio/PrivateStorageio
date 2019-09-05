60ee50c3778baff9c96384eb8e217287c2f07eb8
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-04 20:12:37 -0400

    enable tahoe-lafs

--- morph/staging002.nix
+++ morph/staging002.nix
@@ -8,7 +8,10 @@ in
       ./staging002-hardware.nix
       # Configure it as a system operated by 100TB.
       ../nixos/modules/100tb.nix
-    ];
+      # Bring in our module for configuring the Tahoe-LAFS service and other
+      # Private Storage-specific things.
+      ../nixos/modules/private-storage.nix
+   ];
 
   # Pass the configuration specific to this host to the 100TB module to be
   # expanded into a complete system configuration.  See the 100tb module for
@@ -17,6 +20,15 @@ in
   # The module name is quoted because `1` makes `100tb` look an awful lot like
   # it should be a number.
   "100tb".config = cfg;
+
+  # Turn on the Private Storage (Tahoe-LAFS) service.
+  services.private-storage =
+  { enable = true;
+    # Get the public IPv4 address from the node configuration.
+    inherit (cfg) publicIPv4;
+    # And the port to operate on is specified via parameter.
+    inherit publicStoragePort;
+  };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
