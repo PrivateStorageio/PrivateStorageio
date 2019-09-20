6c90dfd63e2158863c00591f2e7369042143e654
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-20 13:16:37 -0400

    Prefer the Tahoe-LAFS derivation from the ZKAPAuthorizer source.
    It is better than what we have here and duplicating it is sad.  ZKAPAuthorizer
    also has much tighter binding to Tahoe-LAFS version than anything in this
    repository so it makes sense to let it dictate that detail.

--- nixos/pkgs/tahoe-lafs.nix
+++ nixos/pkgs/tahoe-lafs.nix
@@ -1,11 +1,5 @@
-{ fetchFromGitHub, eliot, tahoelafs, plugins ? [ ] }:
-tahoelafs.overrideAttrs (old:
-{ src = fetchFromGitHub
-  { owner = "tahoe-lafs";
-    repo = "tahoe-lafs";
-    rev = "6c1a37c95188c1d9a877286ef726280a68d38a4b";
-    sha256 = "1fd8b6j52wn04bnvnvysws4c713max6k1592lz4nzyjlhrcwawwh";
-  };
-  propagatedBuildInputs = old.propagatedBuildInputs ++ [ eliot ] ++ plugins;
-  doInstallCheck = false;
-})
+{ fetchFromGitHub, python27Packages }:
+let
+  zkapauthorizer = import ./zkapauthorizer-repo.nix { inherit fetchFromGitHub; };
+in
+  python27Packages.callPackage "${zkapauthorizer}/tahoe-lafs.nix" { }
