13b37242232c2305cdcf2d62570b3fd24c53bc5c
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-20 13:16:06 -0400

    Factor ZKAPAuthorizer source derivation out for easier reuse
    Also bump the version being used.  This will be important in a minute.

---
+++ nixos/pkgs/zkapauthorizer-repo.nix
@@ -0,0 +1,7 @@
+{ fetchFromGitHub }:
+fetchFromGitHub {
+  owner = "PrivateStorageio";
+  repo = "ZKAPAuthorizer";
+  rev = "36dd4c2cffa2e9df651dda4c9ac8977bafe2ed64";
+  sha256 = "sha256:1i5nli73gk56r5brmimcd97dkf7wd4mf6viw4vbcssa7xj6s84af";
+}
--- nixos/pkgs/zkapauthorizer.nix
+++ nixos/pkgs/zkapauthorizer.nix
@@ -1,12 +1,5 @@
-{ pkgs, fetchFromGitHub, tahoe-lafs }:
+{ fetchFromGitHub, python27Packages }:
 let
-  src = fetchFromGitHub
-  { owner = "PrivateStorageio";
-    repo = "ZKAPAuthorizer";
-    rev = "a14b38f39e48d1560ea10ec26fffad6ce50fd00a";
-    sha256 = "1v81l0ylx8r8xflhi16m8hb1dm3rlzyfrldiknvggqkyi5psdja4";
-  };
+  zkapauthorizer = import ./zkapauthorizer-repo.nix { inherit fetchFromGitHub; };
 in
-pkgs.python27Packages.callPackage "${src}/zkapauthorizer.nix"
-{ inherit tahoe-lafs;
-}
+  python27Packages.callPackage "${zkapauthorizer}/zkapauthorizer.nix" { }
