4446a35541ee2189b34dbf352339aa369c2f2a19
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-05-30 11:05:34 -0400

    Fix incorrect fixed-derivation hash.
    At least, I suppose it was incorrect.  It won't build anywhere it has not
    already built.  I don't know how it built where it already built.

--- nixos/pkgs/zkapauthorizer-repo.nix
+++ nixos/pkgs/zkapauthorizer-repo.nix
@@ -5,5 +5,5 @@ in
     owner = "PrivateStorageio";
     repo = "ZKAPAuthorizer";
     rev = "27a2f31e5483fa732785cf550e3beef09d67c398";
-    sha256 = "0chd3x5zvv873jah9z0dj4fpq4ika7i919dfzx53ckf395y69dzm";
+    sha256 = "10x28f1iplhskbaqxqcd68kz0llssvn261b87x1aaay3959s8ama";
   }
