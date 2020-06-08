9f4ade015eb82979e69518da75268f03cd104246
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-06-08 15:53:54 -0400

    Remove this no-longer used configuration.

--- morph/grid/production/storage001-config.nix
+++ morph/grid/production/storage001-config.nix
@@ -5,5 +5,4 @@
   "gateway" = "176.113.72.37";
   "gatewayInterface" = "eno1";
   "grubDeviceID" = "wwn-0x5000cca25cc08d27";
-  "rootPublicKey" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN4GenAY/YLGuf1WoMXyyVa3S9i4JLQ0AG+pt7nvcLlQ exarkun@baryon";
 }
--- morph/grid/production/storage004-config.nix
+++ morph/grid/production/storage004-config.nix
@@ -5,5 +5,4 @@
   "gateway" = "87.101.93.197";
   "gatewayInterface" = "eno1";
   "grubDeviceID" = "wwn-0x5000cca249d45533";
-  "rootPublicKey" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN4GenAY/YLGuf1WoMXyyVa3S9i4JLQ0AG+pt7nvcLlQ exarkun@baryon";
 }
