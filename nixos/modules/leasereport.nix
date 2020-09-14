0bd1b19fde5e28a786d2331d0f476c279b381a26
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-09-14 09:19:37 -0400

    that was a garbage revision apparently ...
    I don't know

--- nixos/modules/leasereport.nix
+++ nixos/modules/leasereport.nix
@@ -7,6 +7,6 @@ let
   # is known to work with LeaseReport.  It could be bumped if necessary but
   # this would probably only happen as a result of bumping the resolver in
   # stack.yaml.
-  haskellNixSrc = builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/31fd01e14729e686de0a915b9bc4ff70397a84c7.tar.gz;
+  haskellNixSrc = builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/f6663a8449f5e4a7393aa24601600c8f6e352c97.tar.gz;
 in
   (callPackage "${leasereport}/nix" { inherit haskellNixSrc; }).LeaseReport.components.exes.LeaseReport
