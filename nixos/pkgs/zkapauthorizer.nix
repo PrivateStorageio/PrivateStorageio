330d14b642680b3822b267c3335cc4553722b03e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-09-16 14:04:18 -0400

    Simplify our helper zkapauthorizer.nix package
    Rely on upstream package to supply typing and challenge-bypass-ristretto as
    necessary.  Simplify our expression (approximately pointfree style) so we
    don't have to know about any other arguments that might be passed or accepted.

--- nixos/pkgs/zkapauthorizer.nix
+++ nixos/pkgs/zkapauthorizer.nix
@@ -1,9 +1,1 @@
-{ callPackage }:
-let
-  repo = import ./zkapauthorizer-repo.nix;
-  typing = callPackage "${repo}/typing.nix" { };
-  challenge-bypass-ristretto = callPackage "${repo}/python-challenge-bypass-ristretto.nix" { };
-in
-(callPackage "${repo}/zkapauthorizer.nix" {
-    inherit challenge-bypass-ristretto;
-    }).overrideAttrs (old: { doCheck = false; doInstallCheck = false; })
+import "${import ./zkapauthorizer-repo.nix}/default.nix"
