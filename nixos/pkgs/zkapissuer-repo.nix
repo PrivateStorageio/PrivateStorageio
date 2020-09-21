de2e524daa6533d58993c6e229cc53a5072e4d79
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-09-21 11:40:11 -0400

    Bump to PaymentServer with working Nix packaging

--- nixos/pkgs/zkapissuer-repo.nix
+++ nixos/pkgs/zkapissuer-repo.nix
@@ -4,6 +4,6 @@ in
   pkgs.fetchFromGitHub {
     owner = "PrivateStorageio";
     repo = "PaymentServer";
-    rev = "025e51718c6267916195d06eb3df434edcfcd169";
-    sha256 = "1gqgmhv1dvpvapp9v54qqmx7vkl8ln7f191p7mpf32ndqqnzjnjy";
+    rev = "354d174f641e2e433c72dde8eb12c9fbaeb875a4";
+    sha256 = "0mv3q8pgic0cpdzx19w296d29cwamwv0pjxb8z6p01dj2jf94zzs";
   }
