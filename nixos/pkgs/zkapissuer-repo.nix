b9ba6eccf6bd1b2588a181c6e67e0412583ed8d6
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-09-22 14:48:07 -0400

    Bump to PaymentServer master w/ metric changes

--- nixos/pkgs/zkapissuer-repo.nix
+++ nixos/pkgs/zkapissuer-repo.nix
@@ -4,6 +4,6 @@ in
   pkgs.fetchFromGitHub {
     owner = "PrivateStorageio";
     repo = "PaymentServer";
-    rev = "04a0b325958be64750c3c75272edf9dfc89d4139";
+    rev = "18f3d56e2ac795da5eb5f8ecf01a93f005ce478b";
     sha256 = "1bw3yz971j3rr16pb9cj3jyka3akc8kdx8fv5zlfyvhnm67gdlqx";
   }
