d0ab7ad29025622a3f6e7f5270a33e457a9cd07f
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-09-22 08:16:54 -0400

    bump for test fixes

--- nixos/pkgs/zkapissuer-repo.nix
+++ nixos/pkgs/zkapissuer-repo.nix
@@ -4,6 +4,6 @@ in
   pkgs.fetchFromGitHub {
     owner = "PrivateStorageio";
     repo = "PaymentServer";
-    rev = "00265d835ff11ef9b2f10408dff431e6f04e281a";
-    sha256 = "0ikqqnjbka06d92p60imzh2hkk1gp54g4gpyy5va4s659j1ws1y7";
+    rev = "a83bca9b62ab6b80112116e26bb83d9beb69e015";
+    sha256 = "1i4z7gqyrv21p6v99aaf1vk849n47c30ffs5y3nmmrb2pv3gx6iv";
   }
