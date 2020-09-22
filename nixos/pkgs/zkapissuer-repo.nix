3c8f23d4bf5df563af9bee9698d4170e28fb58ac
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-09-22 14:32:25 -0400

    bump PaymentServer for redemption metric

--- nixos/pkgs/zkapissuer-repo.nix
+++ nixos/pkgs/zkapissuer-repo.nix
@@ -4,6 +4,6 @@ in
   pkgs.fetchFromGitHub {
     owner = "PrivateStorageio";
     repo = "PaymentServer";
-    rev = "a83bca9b62ab6b80112116e26bb83d9beb69e015";
+    rev = "3040a41f70d509e62a6d42f649bbe7b56f97d004";
     sha256 = "1i4z7gqyrv21p6v99aaf1vk849n47c30ffs5y3nmmrb2pv3gx6iv";
   }
